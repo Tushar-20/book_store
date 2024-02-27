ed80ea3b0e7094d87ef6c7e03c7e28318590728d
Tushar <tusharsarker412@gmail.com>
2024-02-27 22:00:02 +0600

    Creating model for my project

--- book/views.py
+++ book/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from book.forms import BookStroreForm
 
 # Create your views here.
 def home(request):
       return render(request, "store_book.html")
+
+def store_book(request):
+      book=BookStroreForm
+      return render(request, 'store_book.html',{'form' :book})
