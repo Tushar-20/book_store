eafba73c771b7d92e58ffabb2b3a82d6fd1db140
Tushar <tusharsarker412@gmail.com>
2024-02-29 09:46:42 +0600

    Create books and Saving to database

--- book/models.py
+++ book/models.py
@@ -14,5 +14,5 @@ class BookStoreModel(models.Model):
       author =models.CharField(max_length=30)
       category = models.CharField(max_length=30, choices=CATEGORY)
       first_pub = models.DateTimeField(auto_now_add=True)
-      last_pub = models.DateTimeField()
+      last_pub = models.DateTimeField(auto_now=True)
       
--- book/views.py
+++ book/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from book.forms import BookStroreForm
-
+from book.models import BookStoreModel
 # Create your views here.
 def home(request):
       return render(request, "store_book.html")
 
 def store_book(request):
-      book=BookStroreForm
-      return render(request, 'store_book.html',{'form' :book})
+      if request.method == 'POST':
+            book = BookStroreForm(request.POST)
+            if book.is_valid():
+                  book.save()
+                  print(book.cleaned_data)
+                  return redirect("show_books")     
+      else:
+            book=BookStroreForm()
+      return render(request, 'store_book.html', {'form' :book})
+def show_books(request):
+      book = BookStoreModel.objects.all()
+      print(book)
+      return render(request,'show_book.html', {'data' : book})
